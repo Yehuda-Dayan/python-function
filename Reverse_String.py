b8d5bf1ea390fc484608a4fbb4223052a872d86b
Yehuda Dayan <dyehuda10@gmail.com>
2025-04-27 11:05:10 +0300

    optimus prime

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,10 +1,5 @@
 def reverse_string(s):
-    """
-    This function takes a string as input and returns the string reversed.
-    
-    :param s: str - The string to be reversed
-    :return: str - The reversed string
-    """
+
     return s[::-1]
 
 string = str(input("Enter a string to reverse: "))
